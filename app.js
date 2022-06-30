e5b0fa42d7a1f626a357eb56ecfb1d75ff003aa6
choijuhwan8 <choijuhwan8@gmail.com>
2022-07-01 02:25:54 +0900

    computed properties

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
 const app = Vue.createApp({
   data() {
-    return { userInput: '', confirmedInput: '' };
+    return {
+      counter: 0,
+      name: '',
+      lastname: '',
+      //fullname: ''
+    };
+  },
+  watch: {
+    counter(value){
+      if(value > 50){
+        const that = this;
+        setTimeout(function(){
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+    // name(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }
+    //   else{
+    //   this.fullname = value + ' ' + this.lastname;
+    //   }
+    // },
+    // lastname(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }
+    //   else{
+    //   this.fullname = this.name + ' ' + 'value';
+    //   }
+    // }
+  },
+  computed: {
+    fullname() {
+      console.log('running...');
+      if(this.name === '' || this.lastname === ''){
+        return '';
+      }else{
+      return this.name + ' ' + this.lastname; 
+    }
+    }
   },
   methods: {
-    showAlert() {
-      alert('This works!');
+    setName(event) {
+      this.name = event.target.value;
     },
-    saveInput(event) {
-      this.userInput = event.target.value;
+    add(num) {
+      this.counter = this.counter + num;
     },
-    confirmInput() {
-      this.confirmedInput = this.userInput;
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
     }
   }
 });
 
-app.mount('#assignment');
+app.mount('#events');
