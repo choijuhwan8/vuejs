c5360ae14411167bf754baad01ab1646ed1d1eae
choijuhwan8 <choijuhwan8@gmail.com>
2022-07-18 22:30:27 +0800

    vue components

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const app = Vue.createApp({
-  data() {
+  data(){
     return {
-      enteredGoalValue: '',
-      goals: []
-    };
+      friends: [
+        {id: 'manuel', name: 'Manuel Lorenz', phone: '01099638922', email: 'manuel@gmail.com'},
+        {id: 'julie', name: 'Julie Jones', phone: '01083837272', email: 'julie@gmail.com'}
+      ]
+    }
   },
   methods: {
-    addGoal() {
-      this.goals.push(this.enteredGoalValue);
-    },
-    removeGoal(index) {
-      this.goals.splice(index, 1);
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
     }
   }
-  
 });
 
-app.mount('#user-goals');
+app.component('friend-contact', {
+  template: `
+        <li>
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend:{id: 'manuel', name: 'Manuel Lorenz', phone: '01099638922', email: 'manuel@gmail.com'}
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+});
+app.mount('#app');
